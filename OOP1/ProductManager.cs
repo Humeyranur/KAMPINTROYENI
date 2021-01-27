d0e04daa8ec3b8fe60fcef56c8ae614024ae7a74
Humeyranur <humeyranurturgut.ie@gmail.com>
2021-01-27 23:44:27 +0300

    ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //manager ile bir isimlendirme görürsen anlaki içerisinde ürünle ilgili operasyonlar var.
+        public void Add()
+        {
+            
+        }
+    }
+}
